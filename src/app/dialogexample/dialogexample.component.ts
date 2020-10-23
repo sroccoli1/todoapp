ec3bb34f1660024165e68a32a713c22f63c6f4a0
sroccoli1 <samuel.roccoli@laposte.net>
2020-10-23 16:36:01 +0300

    Linked button to laucnch the dialog, service to manage TODOs, the dialog

--- src/app/dialogexample/dialogexample.component.ts
+++ src/app/dialogexample/dialogexample.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Todo } from '../todo.model';
+import { ManagetodoService } from '../managetodo/managetodo.service';
 
 @Component({
   selector: 'app-dialogexample',
@@ -11,9 +13,32 @@ export class DialogexampleComponent implements OnInit{
 
   constructor( 
     public dialogRef: MatDialogRef<DialogexampleComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Todo ) { }
+    @Inject(MAT_DIALOG_DATA) public data: Todo,
+    private fb: FormBuilder,
+    public todoService: ManagetodoService) { }
     
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createtodoForm = this.fb.group({
+    todoName : [''],
+    todoDescription: ['']
+  })
+
+  /** Add the todo  */ 
+  addTodo(){
+    // Print the from in the console
+    console.log(this.createtodoForm);
+    // Get the input values and create an instance of Todo
+    let newTodoName = this.createtodoForm.get('todoName').value;
+    let newTodoDescription = this.createtodoForm.get('todoDescription').value;
+    let newTodo = new Todo(newTodoName, newTodoDescription);
+    // Add the todo instance 
+    this.todoService.addTodo(newTodo);
   }
+
+  delete(){
+    this.createtodoForm.reset();
+  }
+
   
 }
